2d214431d84179da49b193d9ba0bf691c15e9e58
Sebastian Manyama <69379857+ishiApp@users.noreply.github.com>
2024-08-22 11:58:09 +0300

    Add files via upload

---
+++ challenge-01/solutions/SebastianChallenge.java
@@ -0,0 +1,16 @@
+public class SebastianChallenge {
+
+    public static void main(String[] args){
+
+        SebastianChallenge sebastianChallenge = new SebastianChallenge();
+        System.out.println("the total is " + sebastianChallenge.total);
+    }
+    public SebastianChallenge(){
+
+    }
+    int number_one = 20;
+    int number_two = 20;
+
+    int total = number_one + number_two;
+}
+
